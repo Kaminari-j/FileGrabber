9006fd97e093082e980fc320354e7e6f9782ab23
Joo Suhong <rodney.joo@gmail.com>
2019-11-17 21:23:39 +0900

    Update example.py

--- example.py
+++ example.py
@@ -3,5 +3,7 @@ from FileGrabber import filegrabber
 
 if __name__ == '__main__':
     # Todo: file saving folder
-    testurl = r'https://www.clien.net/service/board/park/14155846?po=0&sk=title&sv=gif&groupCd=&pt=0'
-    filegrabber.do_grab(testurl, False)
+    testurl = r'https://www.clien.net/service/board/park/14290602'
+    testurl = input('URL: ').strip()
+    filegrabber.do_grab(testurl, True)
+
